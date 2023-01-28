e8f9532047cd4cb37c7ef6a46cc1e46bf5f6efa9
Ivan Yakovlev <mail.siiix@yandex.ru>
2023-01-28 20:28:02 +0300

    Написал код

--- Program.cs
+++ Program.cs
@@ -5,3 +5,30 @@
 //44 5 78 -> 78
 //22 3 9 -> 22
 
+Console.Clear();
+
+Console.Write("Введите первое число: "); 
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: "); 
+int num2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите третье число: "); 
+int num3 = int.Parse(Console.ReadLine());
+
+int max = 0;
+
+if (num1>num2){
+  max=num1;
+}
+else {
+  max=num2;}
+int megamax=0;
+if (max>num3){
+  megamax=max;
+}
+else {
+  megamax=num3;}
+
+
+Console.WriteLine($"Между числами {num1}, {num2} и {num3} - максимальное: {megamax}");
